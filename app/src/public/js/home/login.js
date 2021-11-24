16524e3f3d4ecd69698e3c148a24e2839d71fb95
Seongeuniii <kse8538@naver.com>
2021-11-25 00:43:18 +0900

    frontend login

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -15,7 +15,16 @@ function login() {
     },
     body: JSON.stringify(req)
   }).then((res) => res.json())
-    .then(console.log(res))
+    .then((res) => {
+      if (res.success) {
+        location.href = "/";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error("로그인 중 에러 발생"));
+    })
 }
 
 loginBtn.addEventListener("click", login);
