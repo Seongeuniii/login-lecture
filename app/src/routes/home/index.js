fbd37d971ddca811467907c1b5c48df3e4a65f7a
Seongeuniii <kse8538@naver.com>
2021-12-03 02:38:53 +0900

    Sign Up page

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -9,7 +9,7 @@ const ctrl = require('./home.ctrl'); // MVC 컨트롤러 분리
 
 router.get('/', ctrl.output.home);
 router.get('/login', ctrl.output.login);
-// login data를 받는 API
-router.post('/login', ctrl.process.login);
+router.get('/register', ctrl.output.register);
+router.post('/login', ctrl.process.login);  // login data를 받는 API
 
 module.exports = router;
